7a1bb491d08cf1e17234715b207d7598ce530c58
maurolepore <maurolepore@gmail.com>
2018-05-03 16:39:15 -0400

    Fix CMD check.

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Datasets from __fgeo.data__.
+#'
+#' @examples
+#' str(luquillo_stem_random_tiny, give.attr = FALSE)
+#' str(luquillo_vft_4quad, give.attr = FALSE)
+#' @name luquillo
+NULL
+
+#' @rdname luquillo
+"luquillo_stem_random_tiny"
+
+#' @rdname luquillo
+"luquillo_vft_4quad"
