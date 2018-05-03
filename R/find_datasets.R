879b2c16b7f042bd82d3f456039a4feaea544027
maurolepore <maurolepore@gmail.com>
2018-05-03 17:30:17 -0400

    Add find_datasets() and mat_enframe().

--- R/find_datasets.R
+++ R/find_datasets.R
@@ -1,11 +1,12 @@
 #' Find datasets in a package.
 #'
-#' @param package
+#' @param package String; package name to search.
 #'
-#' @return
+#' @return Sting.
 #' @export
 #'
 #' @examples
+#' find_datasets("fgeo.base")
 find_datasets <- function(package) {
   dinfo <- utils::data(package = package)
   dinfo[["results"]][, "Item"]
