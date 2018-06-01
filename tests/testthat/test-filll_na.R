8623b2889865d560115ffc685f9c389f56ab0131
maurolepore <maurolepore@gmail.com>
2018-06-01 13:40:50 -0400

    Remove tibble from test.

--- tests/testthat/test-filll_na.R
+++ tests/testthat/test-filll_na.R
@@ -1,8 +1,7 @@
 context("fill_na")
 
-df <- tibble::tibble(x = c(NA, 1), y = c("a", NA))
-
 test_that("returns expected value", {
+  df <- data.frame(x = c(NA, 1), y = c("a", NA), stringsAsFactors = FALSE)
   out <- fill_na(df, "z")
   expect_equal(out$x[[1]], "z")
   expect_equal(out$y[[2]], "z")
