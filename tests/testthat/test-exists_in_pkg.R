07679dd821922afbca0c7a1eb9606dec1fca32a6
maurolepore <maurolepore@gmail.com>
2018-08-17 11:25:26 -0400

    Attach datasets to possibly failing test.

--- tests/testthat/test-exists_in_pkg.R
+++ tests/testthat/test-exists_in_pkg.R
@@ -1,6 +1,7 @@
 context("exists_in_pkg")
 
 test_that("is silent or throws error with existing or missing object", {
+  library(datasets)
   expect_silent(exists_in_pkg("cars", "datasets"))
   detach("package:datasets")
   expect_error(exists_in_pkg("cars", "datasets"))
