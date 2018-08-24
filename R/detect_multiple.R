17566437e075fda12bfc509a92d6dd0f678dd590
maurolepore <maurolepore@gmail.com>
2018-08-24 18:27:33 -0400

    Add examples.

--- R/detect_multiple.R
+++ R/detect_multiple.R
@@ -9,9 +9,20 @@
 #' @return A condition (and `.data` invisibly).
 #' @export
 #'
-#' flag_if
-#'
 #' @examples
+#' dupl <- c(1, 1)
+#' flag_if(dupl, is_duplicated)
+#' # Silent
+#' flag_if(dupl, is_multiple)
+#'
+#' mult <- c(1, 2)
+#' flag_if(mult, is_multiple, message, "Custom")
+#' # Silent
+#' flag_if(mult, is_duplicated)
+#'
+#' # Both silent
+#' flag_if(c(1, NA), is_multiple)
+#' flag_if(c(1, NA), is_duplicated)
 flag_if <- function(.data, predicate, condition = warning, msg = NULL) {
   stopifnot(length(condition) == 1)
   if (predicate(.data)) condition(msg %||% "Flagged values were detected.")
